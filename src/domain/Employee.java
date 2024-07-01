eef83ba66bcc03e85d77dde3c4e922ef4ed7d0b3
m0re479 <more_q@mail.ru>
2024-07-01 20:33:15 +0400

    First part of lab

--- src/domain/Employee.java
+++ src/domain/Employee.java
@@ -5,4 +5,38 @@ public class Employee {
     public String name;
     public String ssn;
     public double salary;
+
+    public Employee() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
