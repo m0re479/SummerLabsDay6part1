eeefa6c024e4d04431e77e9c29abed843534d995
m0re479 <more_q@mail.ru>
2024-07-01 20:22:30 +0400

    Init commit

---
+++ src/EmployeeTest.java
@@ -0,0 +1,5 @@
+public class EmployeeTest {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/domain/Employee.java
@@ -0,0 +1,8 @@
+package domain;
+
+public class Employee {
+    public int empId;
+    public String name;
+    public String ssn;
+    public double salary;
+}
